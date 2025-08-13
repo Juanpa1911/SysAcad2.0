45873c0bfd15ecc0b29c13f1b2de8563503d6f28
Carlos <carlosemoya1@gmail.com>
2025-08-13 16:33:36 -0300

    Revert "Clase 6/8/2025"
    This reverts commit 1dc7dc6b4a0a03f8c87b228cebc1e79b51e45156.

--- app/mapping/universidad_mapping.py
+++ app/mapping/universidad_mapping.py
@@ -3,10 +3,10 @@ from app.models import Universidad
 from markupsafe import escape
 
 class UniversidadMapping(Schema):
-    hashid = fields.String(dump_only=True)
+    id = fields.Integer(dump_only=True)
     #TODO agregar validaciones (largo de texto según modelo)
-    nombre = fields.String(required = True, validate = validate.Length(min=1, max=100))
-    sigla = fields.String(required = True, validate = validate.Length(min=1, max=10))
+    nombre = fields.String(required = True)
+    sigla = fields.String(required = True)
 
     @post_load
     def nueva_universidad(self, data, **kwargs):
