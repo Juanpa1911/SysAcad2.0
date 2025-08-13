2a001cf3b352cf1191cc4994e384b524b7c1f459
Carlos <carlosemoya1@gmail.com>
2025-08-13 16:27:25 -0300

    Revert "Clase 6/8/2025"
    This reverts commit 1dc7dc6b4a0a03f8c87b228cebc1e79b51e45156.

--- app/models/universidad.py
+++ app/models/universidad.py
@@ -1,10 +1,8 @@
 from dataclasses import dataclass
 from app import db
-from flask_hashids import HashidMixin
-
 
 @dataclass(init=False, repr=True, eq=True)
-class Universidad(HashidMixin,db.Model):
+class Universidad(db.Model):
     __tablename__ = 'universidades'
     id: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre: str = db.Column(db.String(100), nullable=False)
