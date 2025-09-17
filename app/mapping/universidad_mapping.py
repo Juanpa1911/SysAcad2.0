38214a82e1b735db91f0e4086ed7f02b6d03f3c6
Juan Pablo López Laszuk <120332843+Juanpa1911@users.noreply.github.com>
2025-09-17 15:16:38 -0300

    Mapping universidad
    Se termino bien el mapping de universidad para seguir con los que sigue

--- app/mapping/universidad_mapping.py
+++ app/mapping/universidad_mapping.py
@@ -1,16 +1,12 @@
-from marshmallow import Schema, fields, post_load 
+from marshmallow import Schema, fields, post_load, validate
 from app.models import Universidad
 from markupsafe import escape
 
 class UniversidadMapping(Schema):
     id = fields.Integer(dump_only=True)
-    #TODO agregar validaciones (largo de texto según modelo)
-    nombre = fields.String(required = True)
-    sigla = fields.String(required = True)
+    nombre = fields.String(required = True, validate = validate.Length(min=1, max=100))
+    sigla = fields.String(required = True, validate = validate.Length(min=1, max=10))
 
     @post_load
     def nueva_universidad(self, data, **kwargs):
-        for key, value in data.items():
-            if isinstance(value, str):
-                data[key] = escape(value)
         return Universidad(**data) 
