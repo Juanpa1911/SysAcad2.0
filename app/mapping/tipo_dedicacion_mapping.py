22ddae662738206ba13bc0e5be007f2e32889c0f
Mariano Piastrellini <marianopiastrellini03@gmail.com>
2025-09-23 23:06:33 -0300

    Add Marshmallow mappings for Orientacion and TipoDedicacion models

---
+++ app/mapping/tipo_dedicacion_mapping.py
@@ -0,0 +1,11 @@
+from marshmallow import Schema, fields, post_load
+from app.models import TipoDedicacion
+
+class TipoDedicacionMapping(Schema):
+    id = fields.Int(dump_only=True)
+    nombre = fields.Str(required=True)
+    #TODO no se si va observacion
+
+    @post_load
+    def crear_tipo_dedicacion(self, data, **kwargs):
+        return TipoDedicacion(**data)
