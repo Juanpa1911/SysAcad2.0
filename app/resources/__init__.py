195c26079ad82431f4065d914507532c6f65973f
richardsos <ricardoalberto988@gmail.com>
2025-10-06 20:40:29 -0300

    Hice el resource de area, closes #29

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -3,3 +3,5 @@ from .universidad_resources import universidad_bp
 #from .certificado_resources import certificado_bp
 from .cargo_resources import cargo_bp
 from .alumno_resources import alumno_bp
+from .area_resources import area_bp
+
---
+++ app/resources/area_resources.py
@@ -0,0 +1,42 @@
+from flask import jsonify, Blueprint, request
+from app.services.area_service import AreaService
+from app.mapping.area_mapping import AreaMapping
+
+area_bp = Blueprint('area', __name__)
+area_mapping = AreaMapping()
+
+@area_bp.route('/area', methods=['GET'])
+def buscar_todas():
+    areas = AreaService.buscar_todos()
+    return area_mapping.dump(areas, many=True), 200
+
+@area_bp.route('/area/<int:id>', methods=['GET'])
+def buscar_area_id(id):
+    area = AreaService.buscar_por_id(id)
+    if area:
+        return area_mapping.dump(area), 200
+    return jsonify({"message": "Area no encontrada"}), 404
+
+@area_bp.route('/area', methods=['POST'])
+def crear_area():
+    data = request.get_json()
+    area = area_mapping.load(data)
+    area = AreaService.crear_area(area)
+    return area_mapping.dump(area), 201
+
+@area_bp.route('/area/<int:id>', methods=['PUT'])
+def actualizar_area(id):
+    data = request.get_json()
+    area = area_mapping.load(data)
+    area = AreaService.actualizar_area(area, id)
+    if area:
+        return area_mapping.dump(area), 200
+    return jsonify({"message": "Area no encontrada"}), 404
+
+@area_bp.route('/area/<int:id>', methods=['DELETE'])
+def eliminar_area(id):
+    area = AreaService.borrar_por_id(id)
+    if area:
+        return jsonify({"message": "Area eliminada"}), 200
+    return jsonify({"message": "Area no encontrada"}), 404
+
