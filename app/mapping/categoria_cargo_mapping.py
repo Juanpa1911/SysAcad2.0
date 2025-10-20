7de3b1e620a8ddb34d110a8558e35175f3ad1be1
AnaIriarteLopez <airiartelopez@gmail.com>
2025-10-20 08:57:47 -0300

    Cambié el nombre a CategoriaCargoSchema to CategoriaCargoMapping
    Updated the schema class name in categoria_cargo_mapping.py from CategoriaCargoSchema to CategoriaCargoMapping for consistency or clarity.

--- app/mapping/categoria_cargo_mapping.py
+++ app/mapping/categoria_cargo_mapping.py
@@ -2,7 +2,7 @@ from marshmallow import Schema, fields, post_load, validate
 from app.models import CategoriaCargo
 
 
-class CategoriaCargoSchema(Schema):
+class CategoriaCargoMapping(Schema):
     id = fields.Int(required=True)
     nombre = fields.Str(required=True, validate=validate.Length(min=2, max=100))
     descripcion = fields.Str(required=True, validate=validate.Length(min=2, max=200))
