96c9d6c516791861f3c37b78d1abe2a073a2c1db
Juan Pablo López Laszuk <120332843+Juanpa1911@users.noreply.github.com>
2025-11-23 16:27:21 -0300

    Refactor tipo_especialidad resource to ensure proper data mapping in creation and update endpoints

--- app/resources/tipo_especialidad_resources.py
+++ app/resources/tipo_especialidad_resources.py
@@ -23,7 +23,8 @@ def buscar_tipo_especialidad_por_id(id):
 @tipo_esp_bp.route('/tipo_especialidad', methods=['POST'])
 def crear_tipo_especialidad():
     data = request.get_json()
-    nuevo_tipo_especialidad = TipoEspecialidadService.crear_tipo_especialidad(data)
+    tipo_especialidad = tipo_especialidad_mapping.load(data)
+    nuevo_tipo_especialidad = TipoEspecialidadService.crear_tipo_especialidad(tipo_especialidad)
     return tipo_especialidad_mapping.dump(nuevo_tipo_especialidad), 201
 
 
@@ -31,7 +32,7 @@ def crear_tipo_especialidad():
 def actualizar_tipo_especialidad(id):
     data = request.get_json()
     datos_actualizados = tipo_especialidad_mapping.load(data)
-    tipo_especialidad_actualizado = TipoEspecialidadService.actualizar_tipo_especialidad(datos_actualizados)
+    tipo_especialidad_actualizado = TipoEspecialidadService.actualizar_tipo_especialidad(id, datos_actualizados)
     if tipo_especialidad_actualizado:
         return tipo_especialidad_mapping.dump(tipo_especialidad_actualizado), 200
     return jsonify({"message": "Tipo de especialidad no encontrado"}), 404
